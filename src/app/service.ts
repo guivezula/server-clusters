103d2f1d41350f0958e83aab218360890e65e0fe
Guilherme Vezula <guilherme.vezula@pluritech.com.br>
2020-06-13 13:26:25 -0300

    add remove server method

--- src/app/service.ts
+++ src/app/service.ts
@@ -1,32 +1,52 @@
 import { Injectable } from '@angular/core';
-import { ServerItem } from 'src/app/models';
+import { ServerItem, AppItem } from 'src/app/models';
 
 @Injectable({
   providedIn: 'root'
 })
 export class Service {
 
-  private readonly MIN_CLUSTERS = 4;
+  private readonly MIN_SERVERS = 4;
   private readonly MAX_APPS = 2;
   private servers: ServerItem[] = [];
+  private apps: AppItem[] = [];
 
   constructor() {
-    this.initClusters();
+    this.initServers();
   }
 
-  public addServer() {
-    this.servers.unshift({ type: 'empty' });
-  }
-
-  public getServerList() {
+  public get serverList(): ReadonlyArray<ServerItem> {
     return this.servers;
   }
 
-  private initClusters() {
-    for (let i = 0; i < this.MIN_CLUSTERS; i++) {
+  public get appList(): ReadonlyArray<AppItem> {
+    return this.apps;
+  }
+
+  public addServer() {
+    this.servers.unshift({type: 'empty' });
+  }
+
+  public removeServer() {
+    const emptyClusters = this.servers
+      .filter(item => item.type === 'empty');
+    if (!emptyClusters) {
+      this.servers.splice(0, 1);
+      this.servers.map(() => ({ type: 'empty' }));
+    } else  {
+      const deleted = this.servers.splice(0, 1);
+      if (deleted[0].type !== 'empty') {
+        // TODO insert app
+      }
+    }
+  }
+
+  private initServers() {
+    for (let i = 0; i < this.MIN_SERVERS; i++) {
       this.servers.push({
         type: 'empty',
       });
     }
   }
+
 }
